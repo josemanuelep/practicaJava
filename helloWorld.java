555d1e834550f11e84f3cc60988c58a8bb9b9783
Jose <jose.echeverri@upb.edu.co>
2019-04-02 20:37:06 -0500

    Function isvocal

--- helloWorld.java
+++ helloWorld.java
@@ -2,9 +2,11 @@
 public class HelloWorld {
     public static void main(String args[]) {
 
-        System.out.println("First Java practice for Movil Apps \nNumeros pares");
-        pairNumbers(20, 0);
-        fibonacciNumbers(10);
+        // System.out.println("First Java practice for Movil Apps \nNumeros pares");
+        // pairNumbers(20, 0);
+        // fibonacciNumbers(10);
+        StringBuilder sb = new StringBuilder("hola");
+        deleteVocals(sb);
 
     }
 
@@ -37,8 +39,31 @@ public class HelloWorld {
     }
 
     public static void deleteVocals(StringBuilder text) {
-
+        StringBuilder withoutVocals = new StringBuilder();
         int size = text.length();
+        char t;
+
+        for (int i = 0; i < size; i++) {
+            t = text.charAt(i);
+            System.out.println(t);
+            System.out.println(isVocal(t));
+
+            // if (!isVocal(t)) {
+            // text.setCharAt(i, ' ');
+            // }
+        }
+        System.out.println(text);
+
+    }
+
+    public static boolean isVocal(char c) {
+
+        if (c == 97 || c == 101 || c == 105 || c == 111 || c != 117) {
+            return true;
+        } else if (c == 65 || c == 45 || c == 73 || c == 79 || c == 55) {
+            return true;
+        }
+        return false;
 
     }
 
